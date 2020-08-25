54a557ae6ee747df0158f0be14d3c2e84fa34345
akash77715 <42730452+akash77715@users.noreply.github.com>
2020-08-25 10:15:32 +0530

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -90,26 +90,59 @@
 
   const myQuestions = [
     {
-      question: "This is question 1?",  ///// Write the question inside double quotes
+      question: "Cryptography is all about:",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "Compressing Data",                  ///// Write the option 1 inside double quotes
+        b: "Securing Data",                  ///// Write the option 2 inside double quotes
+        c: "Both",                  ///// Write the option 3 inside double quotes
+        d: "None of the above"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "a"                ///// Write the correct option inside double quotes
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
     },
 
     {
-     question: "This is question 2?",  ///// Write the question inside double quotes
+     question: "XOR stands for:",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "Extended OR",                  ///// Write the option 1 inside double quotes
+        b: "Exclusive OR",                  ///// Write the option 2 inside double quotes
+        c: "Extensible OR",                  ///// Write the option 3 inside double quotes
+        d: "None of the above"                   ///// Write the option 4 inside double quotes
       },
       correctAnswer: "b"                ///// Write the correct option inside double quotes
-    },                                  ///// To add more questions, copy the section below 
+    },
+    
+     {
+      question: "XOR gives true on:",
+      answers: {
+        a: "odd true inputs",
+        b: "even true inputs",
+        c: "all true inputs",
+        d: "all false inputs"
+      },
+      correctAnswer: "a"
+    },
+    
+     {
+      question: "Key is not required in:",
+      answers: {
+        a: "Encryption Algorithm",
+        b: "Hashing Algorithm",
+        c: "Both",
+        d: "None of the above"
+      },
+      correctAnswer: "b"
+    },
+    
+     {
+      question: "What is symmetric-key encryption:",
+      answers: {
+        a: "same key for encryption and decryption",
+        b: "different key for encryption and decryption"
+      },
+      correctAnswer: "a"
+    },
+    
+    ///// To add more questions, copy the section below 
     									                  ///// this line
 
 
